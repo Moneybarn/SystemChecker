724708806da910916665136ff8ab798de41e95c0
Matt Jeanes <matthew.jeanes@moneybarn.com>
2017-04-28 17:46:11 +0100

    Add app files (they were accidentally ignored)

---
+++ src/SystemChecker.Web/wwwroot/app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'system-checker',
+    moduleId: module.id,
+    templateUrl: 'app.template.html'
+})
+export class AppComponent { }
